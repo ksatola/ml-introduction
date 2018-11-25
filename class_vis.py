0eaac7145abb02f14d6d99dadf40e7466969f331
Krzysztof Satola <krzysztof@satola.net>
2018-11-25 15:26:45 +0100

    Update

--- class_vis.py
+++ class_vis.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pylab as pl
 
-def prettyPicture(clf, X_test, y_test):
+def prettyPicture(clf, X_test, y_test, filename="test.png"):
     x_min = 0.0; x_max = 1.0
     y_min = 0.0; y_max = 1.0
     
@@ -33,18 +33,4 @@ def prettyPicture(clf, X_test, y_test):
     plt.xlabel("bumpiness")
     plt.ylabel("grade")
 
-    plt.savefig("test.png")
-
-import base64
-import json
-import subprocess
-
-def output_image(name, format, bytes):
-    image_start = "BEGIN_IMAGE_f9825uweof8jw9fj4r8"
-    image_end = "END_IMAGE_0238jfw08fjsiufhw8frs"
-    data = {}
-    data['name'] = name
-    data['format'] = format
-    data['bytes'] = base64.encodestring(bytes)
-    #print image_start+json.dumps(data)+image_end
-                                    
+    plt.savefig(filename)                                    
